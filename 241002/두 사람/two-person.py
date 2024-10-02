e29c4cf9a753d880391ed5041d11117a1ad845c6
beomdo-park <96810424+beomdo-park@users.noreply.github.com>
2024-10-02 14:50:30 +0900

    Title: 두 사람, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241002/두 사람/two-person.py
@@ -0,0 +1,10 @@
+flag = 0
+
+age, gender = input().rstrip().split()
+if int(age)>=19 and gender=='M':
+    flag = 1
+
+age, gender = input().rstrip().split()
+if int(age)>=19 and gender=='M':
+    flag = 1
+print(flag)
