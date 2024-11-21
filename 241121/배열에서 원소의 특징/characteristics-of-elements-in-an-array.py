e42f35a4b7d310d203778775362e2611e9d9a8be
beomdo-park <96810424+beomdo-park@users.noreply.github.com>
2024-11-21 14:46:43 +0900

    Title: 배열에서 원소의 특징, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241121/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
+++ 241121/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
@@ -1,4 +1,4 @@
 import sys
 
 N = list(map(int, sys.stdin.readline().split()))
-print(next((i for i in range(len(N)) if N[i] % 3 == 0), -1))
+print(N[next((i-1 for i in range(len(N)) if N[i] % 3 == 0), -1)])
