55aa8c632dd46e80a5c86757479fd5a11f232ab7
beomdo-park <96810424+beomdo-park@users.noreply.github.com>
2024-11-21 15:47:25 +0900

    Title: 격자 반대로 채우기, Time: 74ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 241121/격자 반대로 채우기/grid-anti-fill.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+matrix = [[n * i + j + 1 for j in range(n)] for i in range(n)]
+
+#짝수 줄 역순만들기
+for i in range(1, n+1,2):
+    matrix[i]=matrix[i][::-1]
+
+result_matrix = [[0 for i in range(n)] for j in range(n)]
+#행 열 바꿔주기
+for i in range(n):
+    for j in range(n):
+        result_matrix[i][j] = matrix[n-j-1][n-i-1]
+
+for row in result_matrix:
+    print(" ".join(map(str,row)))
