77ffd8bd05a45a895d83ce6f458f411cb2a317e5
beomdo-park <96810424+beomdo-park@users.noreply.github.com>
2025-05-06 01:14:18 +0900

    Title: 최고의 33위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250506/최고의 33위치/best-place-of-33.py
+++ 250506/최고의 33위치/best-place-of-33.py
@@ -3,9 +3,9 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 s=0
-for i in range(n-3):
-    for j in range(n-3):
-        new_grid = grid[i:i+2][j:j+2]
+for i in range(n-2):
+    for j in range(n-2):
+        new_grid = [row[j:j+3] for row in grid[i:i+3]]
         count = sum([row.count(1) for row in new_grid])
         s = s if s>count else count
 print(s)
